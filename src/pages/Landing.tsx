4a6fe5be99c1c8892113901bdbb4826a808604a9
Yureusu <yureusu9@gmail.com>
2025-09-21 13:59:12 +0800

    Initial commit

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -2,6 +2,10 @@ import { useState } from "react";
 import Header from "../components/Header";
 import Showcase from "../components/Showcase";
 import ImageCarousel from "../components/ImageCarousel";
+import Cat1 from "../assets/img/Cat1.jpg"
+import Cat2 from "../assets/img/Cat2.jpg"
+import Cat3 from "../assets/img/Cat3.jpg"
+import Cat4 from "../assets/img/Cat4.jpg"
 
 function Landing() {
 
@@ -12,16 +16,16 @@ function Landing() {
             h-auto w-full flex flex-col items-center justify-start p-[calc(0.6vw+0.4rem)] `}>
             <Header isDark={isDark} setIsDark={setIsDark}/>
             <Showcase isDark={isDark} 
-                image1={{src: "/src/assets/img/Cat1.jpg", width: "240px", height: "240px"}}
+                image1={{src: Cat1, width: "240px", height: "240px"}}
                 description1={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis veritatis nihil sint quibusdam blanditiis, beatae accusamus nisi obcaecati, eum quae maxime aspernatur eius similique iste officia fugit explicabo? Minus, facilis."}
-                image2={{src: "/src/assets/img/Cat2.jpg", width: "240px", height: "240px"}}
+                image2={{src: Cat2, width: "240px", height: "240px"}}
                 description2={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis veritatis nihil sint quibusdam blanditiis, beatae accusamus nisi obcaecati, eum quae maxime aspernatur eius similique iste officia fugit explicabo? Minus, facilis."}            
             />
             <ImageCarousel 
-                image1={{src: "/src/assets/img/Cat1.jpg"}}
-                image2={{src: "/src/assets/img/Cat2.jpg"}}
-                image3={{src: "/src/assets/img/Cat3.jpg"}}
-                image4={{src: "/src/assets/img/Cat4.jpg"}}
+                image1={{src: Cat1}}
+                image2={{src: Cat2}}
+                image3={{src: Cat3}}
+                image4={{src: Cat4}}
             />
         </div>
     )
