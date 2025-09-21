5620f6f926a0779c9a8f9dd3d4f7cb7a2978d864
Yureusu <yureusu9@gmail.com>
2025-09-21 21:53:00 +0800

    Added topnav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import Theme from "./Theme"
+import TopNav from "./TopNav"
 
 type changeTheme = {
     isDark: boolean;
@@ -10,8 +11,30 @@ function Header({isDark, setIsDark}: changeTheme) {
     return (
         <div className={`${isDark? "text-[var(--bg-color)] bg-[var(--text-color)]" : "text-[var(--text-color)] bg-[var(--bg-color)]"}
             h-full w-full flex flex-col items-start justify-evenly px-[calc(0.6vw+0.4rem)] gap-[calc(0.6vw+0.4rem)]`}>
-            <div className="h-auto w-full flex flex-row items-center justify-center">
+            <div className="h-auto w-full flex flex-row items-center justify-between">
                 <Theme isDark={isDark} setIsDark={setIsDark}/>
+                <TopNav titles={[
+                    <div className={`${isDark? "text-[var(--placeholder-color)] bg-[var(--text-color)] hover:text-[var(--bg-color)]" : "text-[var(--placeholder-color)] bg-[var(--bg-color)]  hover:text-[var(--text-color)]"}
+                        flex flex-row items-center justify-center bg-transparent cursor-pointer`}>
+                        <p>Home</p>
+                    </div>,
+                    <div className={`${isDark? "text-[var(--placeholder-color)] bg-[var(--text-color)] hover:text-[var(--bg-color)]" : "text-[var(--placeholder-color)] bg-[var(--bg-color)] hover:text-[var(--text-color)]"}
+                        flex flex-row items-center justify-center gap-[calc(0.4vw+0.2rem)] bg-transparent
+                        cursor-pointer text-[var(--placeholder-color)] hover:text-[var(--text-color)]`}>
+                        <p>Report Lost Item</p>
+                        <i className="fa-solid fa-chevron-down text-[calc(0.5vw+0.3rem)]"></i>
+                    </div>,
+                    <div className={`${isDark? "text-[var(--placeholder-color)] bg-[var(--text-color)] hover:text-[var(--bg-color)]" : "text-[var(--placeholder-color)] bg-[var(--bg-color)] hover:text-[var(--text-color)]"}
+                        flex flex-row items-center justify-center gap-[calc(0.4vw+0.2rem)] bg-transparent
+                        cursor-pointer text-[var(--placeholder-color)] hover:text-[var(--text-color)]`}>
+                        <p>Search Lost Items</p>
+                        <i className="fa-solid fa-chevron-down text-[calc(0.5vw+0.3rem)] bg-transparent"></i>
+                    </div>,
+                    <div className={`${isDark? "text-[var(--placeholder-color)] bg-[var(--text-color)] hover:text-[var(--bg-color)]" : "text-[var(--placeholder-color)] bg-[var(--bg-color)] hover:text-[var(--text-color)]"}
+                        flex flex-row items-center justify-center cursor-pointer text-[var(--placeholder-color)] hover:text-[var(--text-color)]`}>
+                        <p>Contact</p>
+                    </div>
+                ]} isDark={false}/>
             </div>
         </div>
     )
---
+++ src/components/TopNav.tsx
@@ -0,0 +1,19 @@
+import type { JSX } from "react";
+
+type TopNavInfo = {
+    titles: JSX.Element[];
+    isDark: boolean;
+}
+
+const TopNav: React.FC<TopNavInfo> = ({ titles, isDark }) => {
+
+    return(
+        <div title="Top Navigation" className={`flex flex-row items-center justify-center px-[calc(0.6vw+0.4rem)] py-[calc(0.4vw+0.2rem)] gap-[calc(0.6vw+0.4rem)]`}>
+            {titles.map((title, index) => (
+                <div key={index} className={`text-[calc(0.6vw+0.4rem)] cursor-pointer text-[var(--placeholder-color)] hover:text-[var(--text-color)]`}>{title}</div>
+            ))}
+        </div> 
+    )
+}
+
+export default TopNav;
